89654561e06cf0454b850559db7c64ea4f2f0bd8
zy <13162573087@163.com>
2020-04-11 00:30:05 +0800

    add comment function with API

---
+++ src/main/java/com/example/demo/controller/CommentController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.CommentDTO;
+import com.example.demo.mapper.CommentMapper;
+import com.example.demo.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @ResponseBody
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO){
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setType(commentDTO.getType());
+        comment.setContent(commentDTO.getContent());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setGmtModified(comment.getGmtCreate());
+        comment.setCommentator("1");
+        commentMapper.insertSelective(comment);
+        return null;
+    }
+}
